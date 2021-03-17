2834ac96da2151640a42f4d3d80fedb020e9290a
bugpz <13558622779@163.com>
2021-03-17 17:56:35 +0800

    fix：实现简单的登录，目前只能传参数，待解决json传值问题

--- src/main/java/com/example/demo/Comtroller/HelloController.java
+++ src/main/java/com/example/demo/Comtroller/HelloController.java
@@ -1,16 +1,27 @@
 package com.example.demo.Comtroller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+/*import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;*/
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+/*@ResponseBody
+@Controller @RestController可以代替前两个*/
+@RestController  /*这个类的所有方法返回的所有方法直接写给浏览器（如果是对象转为json数据）*/
 public class HelloController {
 
-    @ResponseBody
-    @RequestMapping("/he")
-    public String hello() {
-
+    @PostMapping(value = "/h")
+    public String hello(@RequestParam( "username") String username,
+                        @RequestParam( "password") String password
+                        ) {
+        JSONObject jsonObject = new JSONObject();
+        if (username.equals("bugPZ") && password.equals("123456")){
+            jsonObject.put("username", username);
+            jsonObject.put("password", password);
+            jsonObject.put("code", 200);
+            jsonObject.put("msg", "登录成功");
+            return jsonObject.toJSONString();
+        }
         return "hello, world";
     }
 }
