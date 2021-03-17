3f618e5341345b2b7335f94f66fc83316a1bda5d
bugpz <13558622779@163.com>
2021-03-17 17:57:07 +0800

    fix：代码格式修改

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @SpringBootApplication 标注一个主程序，说明这是一个springboot应用
- */
+/*
+    @SpringBootApplication 标注一个主程序，说明这是一个springboot应用
+*/
 @SpringBootApplication
 public class DemoApplication {
 
