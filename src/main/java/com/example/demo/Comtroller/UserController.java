5780a6d75f803f75f974b00cc16ecfdbfad2b775
bugpz <13558622779@163.com>
2021-03-18 23:19:17 +0800

    add：增加用户表相关文件

---
+++ src/main/java/com/example/demo/Comtroller/UserController.java
@@ -0,0 +1,49 @@
+package com.example.demo.Comtroller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+
+public class UserController {
+
+    @Autowired
+    private UserRepository repository;
+
+    /*
+    * 1.获取所有信息
+    * */
+    @GetMapping("/user")
+    public List<User> getAll(){
+        return repository.findAll();
+    }
+
+    /*
+    * 2.创建一条信息
+    * */
+    @PostMapping("/user")
+    public User create(@RequestParam("username")String username,
+                       @RequestParam("password")String password,
+                       @RequestParam("phone")String phone,
+                       @RequestParam("sex")String sex,
+                       @RequestParam("email")String email){
+
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+        user.setPhone(phone);
+        user.setSex(sex);
+        return repository.save(user);
+    }
+
+    /*
+    * 3.通过id查用户
+    * */
+    @GetMapping("user/{id}")
+    public User findById(@PathVariable("id")Integer id){
+        return repository.findById(id).orElse(null);
+    }
+}
