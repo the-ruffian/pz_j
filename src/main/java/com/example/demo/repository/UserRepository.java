ebbfebcf0fecceaa2df4ffbfcb7b17d1b8f3513d
bugpz <13558622779@163.com>
2021-03-20 17:50:19 +0800

    fix：增加根据手机号查询的接口

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,9 +2,14 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
 
+    @Query(value = "select u from User u where u.phone=?1")
+    Optional<User> findByPhone(String phone);
 
 
 }
