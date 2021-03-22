ff11b872b6477c1b0cbe589b0aa1f6951ad6d071
bugpz <13558622779@163.com>
2021-03-22 10:42:44 +0800

    fix：增加API表字段说明

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,5 +1,8 @@
 package com.example.demo.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,21 +10,32 @@ import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
 @Entity
+@ApiModel(value = "用户")
 public class User {
 
     @Id
     @GeneratedValue
     private Integer id;
+
+    @ApiModelProperty(value = "用户名")
     @NotNull(message = "用户名不能为空")
     private String username;
+
+    @ApiModelProperty(value = "用户密码")
     @NotNull(message = "密码不能为空")
     private String password;
+
+    @ApiModelProperty(value = "电子邮箱")
     @NotNull(message = "邮箱不能为空")
     private String email;
+
+    @ApiModelProperty(value = "性别:中文")
     @NotNull(message = "性别不能为空")
     private String sex;
 
+    @ApiModelProperty(value = "手机号", required = true)
     @Column(length = 12, nullable = false)
+    @NotNull(message = "手机号不能为空")
     private String phone;
 
     public User() {
