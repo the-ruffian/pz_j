38930a96f3b5a9a4d22fc53042c2eebd02114e57
bugpz <13558622779@163.com>
2021-04-03 16:31:18 +0800

    fix：增加根据手机号删除功能

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,8 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
@@ -11,5 +13,9 @@ public interface UserRepository extends JpaRepository<User, Integer>{
     @Query(value = "select u from User u where u.phone=?1")
     Optional<User> findByPhone(String phone);
 
+    @Modifying
+    @Transactional
+    @Query(value = "delete from User where phone=?1")
+    void deleteByPhone(String phone);
 
 }
