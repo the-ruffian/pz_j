081a8bebc6488d931a24239e1e68efb9cb50c358
bugpz <13558622779@163.com>
2021-04-04 17:44:39 +0800

    fix：增加修改token方法

--- src/main/java/com/example/demo/repository/TokenRepository.java
+++ src/main/java/com/example/demo/repository/TokenRepository.java
@@ -2,8 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Integer> {
@@ -12,4 +14,10 @@ public interface TokenRepository extends JpaRepository<Token, Integer> {
 
     @Query(value = "select t from Token t where t.token=?1")
     Optional<Token> findByToken(String token);
+
+    @Modifying
+    @Transactional
+    @Query(value = "update Token set token=?1 where phone=?2")
+    Optional<Token> updateByPhone(String token, String phone);
+
 }
