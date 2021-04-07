530bd61ccd0fde3f98e56f3116edd24e9c54d40f
bugpz <13558622779@163.com>
2021-04-07 23:09:51 +0800

    add：token拦截器

---
+++ src/main/java/com/example/demo/insterceptors/JWTInterceptor.java
@@ -0,0 +1,43 @@
+package com.example.demo.insterceptors;
+
+import com.alibaba.fastjson.JSONObject;
+import com.auth0.jwt.exceptions.AlgorithmMismatchException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.example.demo.utils.JWTUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class JWTInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        JSONObject map = new JSONObject();
+        //获取请求头中的令牌
+        String token = request.getHeader("Authorization");
+        try {
+            JWTUtils.verify(token);
+            return true; //放行请求
+        } catch (SignatureVerificationException e) {
+            e.printStackTrace();
+            map.put("msg", "无效签名!");
+        } catch (TokenExpiredException e) {
+            e.printStackTrace();
+            map.put("msg", "token过期!");
+        } catch (AlgorithmMismatchException e) {
+            e.printStackTrace();
+            map.put("msg","token算法不一致!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            map.put("msg","token无效!");
+        }
+        map.put("state", false);
+        //将map转为json
+        String json = map.toJSONString();
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().println(json);
+        return false;
+    }
+}
