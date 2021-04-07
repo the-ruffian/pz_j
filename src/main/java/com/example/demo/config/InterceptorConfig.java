2ab810923c2ba12cec260f8ca3fb4801c724d3d0
bugpz <13558622779@163.com>
2021-04-07 23:10:55 +0800

    add：拦截器配置

---
+++ src/main/java/com/example/demo/config/InterceptorConfig.java
@@ -0,0 +1,16 @@
+package com.example.demo.config;
+
+import com.example.demo.insterceptors.JWTInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new JWTInterceptor())
+                .addPathPatterns("/**") //token验证接口
+                .excludePathPatterns("/api/user/login","/api/user/register"); // 放行
+    }
+}
