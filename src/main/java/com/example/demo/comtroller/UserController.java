d3df5f23d470f261897e32b4bf45242fdf454242
bugpz <13558622779@163.com>
2021-05-24 17:12:46 +0800

    fix：因用mybatis-plus替换jpa，所有逻辑实现修改

--- src/main/java/com/example/demo/comtroller/UserController.java
+++ src/main/java/com/example/demo/comtroller/UserController.java
@@ -2,16 +2,20 @@ package com.example.demo.comtroller;
 
 
 import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.demo.entity.Token;
 import com.example.demo.entity.User;
 import com.example.demo.repository.TokenRepository;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.utils.JWTUtils;
+import com.example.demo.vo.UserVo;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Wrapper;
 import java.util.*;
 
 @RestController
@@ -28,157 +32,170 @@ public class UserController {
     * */
     @ApiOperation(value = "获取所有用户信息")
     @GetMapping("/api/user")
-    public Object getAll(){
-        List<User> userList = repository.findAll();
-        JSONObject obj = new JSONObject();
-        obj.put("result", userList);
-        obj.put("code",200);
-        return obj.toJSONString();
+//    public Object getAll(){
+//        List<User> userList = repository.selectList();
+//        JSONObject obj = new JSONObject();
+//        obj.put("result", userList);
+//        obj.put("code",200);
+//        return obj.toJSONString();
+//    }
+    public UserVo queryList(Integer current, Integer size) {
+        /**
+         * 这些代码应该写在service层
+         */
+        UserVo userVo = new UserVo();
+        IPage<User> page = new Page<>(current, size); //current 当前页 size 每页个数
+        repository.selectPage(page, null);
+        userVo.setCurrent(current);
+        userVo.setSize(size);
+        userVo.setTotal(page.getTotal());
+        userVo.setUserList(page.getRecords());
+        return userVo;
     }
 
     /*
     * 2.创建一条信息
     * */
-    @ApiOperation(value = "用户注册")
-    @PostMapping(value = "/api/user/register", produces = "application/json;charset=UTF-8")
-    public Object create(@RequestBody JSONObject jsonParam){
-        String username = jsonParam.getString("username");
-        String password = jsonParam.getString("password");
-        String email = jsonParam.getString("email");
-        String phone = jsonParam.getString("phone");
-        String sex = jsonParam.getString("sex");
-        String md5Password = DigestUtils.md5DigestAsHex(password.getBytes());
-        Optional<User> userParam = repository.findByPhone(phone);
-        JSONObject obj = new JSONObject();
-        if (userParam.isPresent()){
-            obj.put("code",400);
-            obj.put("msg","该手机号已注册");
-        } else {
-        User user = new User();
-        user.setUsername(username);
-        user.setPassword(md5Password);
-        user.setEmail(email);
-        user.setPhone(phone);
-        user.setSex(sex);
-        repository.save(user);
-        obj.put("msg", "尊敬的" + username + "用户，恭喜你注册成功");
-        obj.put("code",200);
-        }
-        return obj.toJSONString();
-    }
+//    @ApiOperation(value = "用户注册")
+//    @PostMapping(value = "/api/user/register", produces = "application/json;charset=UTF-8")
+//    public Object create(@RequestBody JSONObject jsonParam){
+//        String username = jsonParam.getString("username");
+//        String password = jsonParam.getString("password");
+//        String email = jsonParam.getString("email");
+//        String phone = jsonParam.getString("phone");
+//        String sex = jsonParam.getString("sex");
+//        String md5Password = DigestUtils.md5DigestAsHex(password.getBytes());
+//        Optional<User> userParam = repository.findByPhone(phone);
+//        JSONObject obj = new JSONObject();
+//        if (userParam.isPresent()){
+//            obj.put("code",400);
+//            obj.put("msg","该手机号已注册");
+//        } else {
+//        User user = new User();
+//        user.setUsername(username);
+//        user.setPassword(md5Password);
+//        user.setEmail(email);
+//        user.setPhone(phone);
+//        user.setSex(sex);
+//        repository.save(user);
+//        obj.put("msg", "尊敬的" + username + "用户，恭喜你注册成功");
+//        obj.put("code",200);
+//        }
+//        return obj.toJSONString();
+//    }
 
     /*
     * 3.通过手机号查用户
     * */
-    @ApiOperation(value = "根据手机号获取用户")
-    @GetMapping("/api/user/{phone}")
-    public Object findByPhone(@PathVariable("phone")String phone){
-        Optional<User> userList = repository.findByPhone(phone);
-        User user;
-        JSONObject obj = new JSONObject();
-        if (userList.isPresent()){
-            user = userList.get();
-            obj.put("code",200);
-            obj.put("result", user);
-        }   else {
-            obj.put("code", 400);
-            obj.put("msg", "没有此用户");
-        }
-        return obj.toJSONString();
-    }
+//    @ApiOperation(value = "根据手机号获取用户")
+//    @GetMapping("/api/user/{phone}")
+//    public Object findByPhone(@PathVariable("phone")String phone){
+//        Optional<User> userList = repository.findByPhone(phone);
+//        User user;
+//        JSONObject obj = new JSONObject();
+//        if (userList.isPresent()){
+//            user = userList.get();
+//            obj.put("code",200);
+//            obj.put("result", user);
+//        }   else {
+//            obj.put("code", 400);
+//            obj.put("msg", "没有此用户");
+//        }
+//        return obj.toJSONString();
+//    }
     /*
     *  修改用户数据
     * */
-    @ApiOperation(value = "修改用户信息")
-    @ApiParam(value = "{test:test}")
-    @PutMapping(value = "/api/user/{phone}", produces = "application/json;charset=UTF-8")
-    public Object update(@PathVariable("phone") String phone,
-                       @RequestBody JSONObject jsonParam){
-        String username = jsonParam.getString("username");
-        String email = jsonParam.getString("email");
-        Optional<User> userList= repository.findByPhone(phone);
-        System.out.println(userList);
-        JSONObject obj = new JSONObject();
-        if (userList.isPresent()
-        && username != null && username.length() > 0
-        && email != null && email.length() > 0){
-           repository.updateByPhone(email, username, phone);
-            obj.put("msg","资料修改成功");
-            obj.put("code", 200);
-        } else if (username.length() <= 0){
-            obj.put("msg","修改失败，username不能为空");
-            obj.put("code", 401);
-        } else if (email.length() <=0) {
-            obj.put("msg","修改失败，email不能为空");
-            obj.put("code", 401);
-        }
-        return obj.toJSONString();
-    }
+//    @ApiOperation(value = "修改用户信息")
+//    @ApiParam(value = "{test:test}")
+//    @PutMapping(value = "/api/user/{phone}", produces = "application/json;charset=UTF-8")
+//    public Object update(@PathVariable("phone") String phone,
+//                       @RequestBody JSONObject jsonParam){
+//        String username = jsonParam.getString("username");
+//        String email = jsonParam.getString("email");
+//        Optional<User> userList= repository.findByPhone(phone);
+//        System.out.println(userList);
+//        JSONObject obj = new JSONObject();
+//        if (userList.isPresent()
+//        && username != null && username.length() > 0
+//        && email != null && email.length() > 0){
+//           repository.updateByPhone(email, username, phone);
+//            obj.put("msg","资料修改成功");
+//            obj.put("code", 200);
+//        } else if (username.length() <= 0){
+//            obj.put("msg","修改失败，username不能为空");
+//            obj.put("code", 401);
+//        } else if (email.length() <=0) {
+//            obj.put("msg","修改失败，email不能为空");
+//            obj.put("code", 401);
+//        }
+//        return obj.toJSONString();
+//    }
 
     /*
     * 删除用户
     * */
-    @ApiOperation(value = "删除账号")
-    @DeleteMapping("/api/user/delete/{phone}")
-    public Object delete(@PathVariable("phone")String phone){
-        Optional<User> isUser = repository.findByPhone(phone);
-        JSONObject obj = new JSONObject();
-        if (isUser.isPresent()){
-            obj.put("msg","删除成功");
-            obj.put("code", 200);
-            repository.deleteByPhone(phone);
-        } else {
-            obj.put("msg", "删除失败");
-            obj.put("code", 400);
-        }
+//    @ApiOperation(value = "删除账号")
+//    @DeleteMapping("/api/user/delete/{phone}")
+//    public Object delete(@PathVariable("phone")String phone){
+//        Optional<User> isUser = repository.findByPhone(phone);
+//        JSONObject obj = new JSONObject();
+//        if (isUser.isPresent()){
+//            obj.put("msg","删除成功");
+//            obj.put("code", 200);
+//            repository.deleteByPhone(phone);
+//        } else {
+//            obj.put("msg", "删除失败");
+//            obj.put("code", 400);
+//        }
+//
+//        return obj.toJSONString();
+//    }
 
-        return obj.toJSONString();
-    }
-
-    @ApiOperation("用户登录")
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "请求成功"),
-            @ApiResponse(code = 400, message = "请求参数没填好"),
-            @ApiResponse(code = 404, message = "请求路径没有或页面跳转路径不对")
-    })
-    @PostMapping(value = "/api/user/login", produces = "application/json;charset=UTF-8")
-    public Object login(@RequestBody JSONObject jsonParam){
-        System.out.println(jsonParam);
-        String phone = jsonParam.getString("phone");
-        String password = jsonParam.getString("password");
-        String md5Password = DigestUtils.md5DigestAsHex(password.getBytes());
-        System.out.println(phone + password);
-        Optional<User> userList = repository.findByPhone(phone);
-        User user;
-        JSONObject obj = new JSONObject();
-        if (userList.isPresent()){
-            user = userList.get();
-            if (md5Password.equals(user.getPassword())){
-                Map<String, String> payload = new HashMap<>();
-                payload.put("phone", phone);
-                payload.put("md5Password", md5Password);
-                String token = JWTUtils.getToken(payload);
-                Optional<Token> tokenList = repositoryT.findByPhone(phone);
-                if (tokenList.isPresent()){
-                    repositoryT.updateByPhone(token,phone);
-                } else {
-                    Token token1 = new Token();
-                    token1.setPhone(phone);
-                    token1.setToken(token);
-                    repositoryT.save(token1);
-                }
-                obj.put("code", 200);
-                obj.put("msg", "登录成功");
-                obj.put("token", token);
-                obj.put("username", user.getUsername());
-            } else {
-                obj.put("code", 201);
-                obj.put("msg", "密码错误");
-            }
-        }else {
-            obj.put("code",401);
-            obj.put("msg","账号不存在");
-        }
-        return obj.toJSONString();
-    }
+//    @ApiOperation("用户登录")
+//    @ApiResponses({
+//            @ApiResponse(code = 200, message = "请求成功"),
+//            @ApiResponse(code = 400, message = "请求参数没填好"),
+//            @ApiResponse(code = 404, message = "请求路径没有或页面跳转路径不对")
+//    })
+//    @PostMapping(value = "/api/user/login", produces = "application/json;charset=UTF-8")
+//    public Object login(@RequestBody JSONObject jsonParam){
+//        System.out.println(jsonParam);
+//        String phone = jsonParam.getString("phone");
+//        String password = jsonParam.getString("password");
+//        String md5Password = DigestUtils.md5DigestAsHex(password.getBytes());
+//        System.out.println(phone + password);
+//        Optional<User> userList = repository.findByPhone(phone);
+//        User user;
+//        JSONObject obj = new JSONObject();
+//        if (userList.isPresent()){
+//            user = userList.get();
+//            if (md5Password.equals(user.getPassword())){
+//                Map<String, String> payload = new HashMap<>();
+//                payload.put("phone", phone);
+//                payload.put("md5Password", md5Password);
+//                String token = JWTUtils.getToken(payload);
+//                Optional<Token> tokenList = repositoryT.findByPhone(phone);
+//                if (tokenList.isPresent()){
+//                    repositoryT.updateByPhone(token,phone);
+//                } else {
+//                    Token token1 = new Token();
+//                    token1.setPhone(phone);
+//                    token1.setToken(token);
+//                    repositoryT.save(token1);
+//                }
+//                obj.put("code", 200);
+//                obj.put("msg", "登录成功");
+//                obj.put("token", token);
+//                obj.put("username", user.getUsername());
+//            } else {
+//                obj.put("code", 201);
+//                obj.put("msg", "密码错误");
+//            }
+//        }else {
+//            obj.put("code",401);
+//            obj.put("msg","账号不存在");
+//        }
+//        return obj.toJSONString();
+//    }
 }
