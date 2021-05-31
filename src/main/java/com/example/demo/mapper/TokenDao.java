39172b2c51d1fdf295d086f7e2c7950f449d8850
bugpz <13558622779@163.com>
2021-05-31 09:40:15 +0800

    fix：加@Component注解 解决 No beans of 'xxxx' type found提示

--- src/main/java/com/example/demo/mapper/TokenDao.java
+++ src/main/java/com/example/demo/mapper/TokenDao.java
@@ -2,7 +2,9 @@ package com.example.demo.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.demo.entity.Token;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface TokenDao extends BaseMapper<Token> {
 
 }
--- src/main/java/com/example/demo/mapper/UserDao.java
+++ src/main/java/com/example/demo/mapper/UserDao.java
@@ -2,7 +2,9 @@ package com.example.demo.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.demo.entity.User;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface UserDao extends BaseMapper<User> {
 
 }
