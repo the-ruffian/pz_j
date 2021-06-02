5192d9b0d77a97d9efe7d744d909a3ec8e9ef22e
bugpz <13558622779@163.com>
2021-06-02 10:34:17 +0800

    add：角色权限表实例

---
+++ src/main/java/com/example/demo/entity/Role_permission.java
@@ -0,0 +1,72 @@
+/*
+ * @Description:Role_permission
+ * @CreatedBy:IntelliJ IDEA
+ * @Author: the-ruffian
+ * @Date: 2021-06-02 10:31
+ * @LastEditTime: 2021-06-02 10:31
+ * @LastEditors: the-ruffian
+ */
+package com.example.demo.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@TableName(value = "role_permission")
+public class Role_permission {
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    private Integer role_id;
+
+    private Integer permission_id;
+
+    private Date create_time;
+
+    private Date update_time;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(Integer role_id) {
+        this.role_id = role_id;
+    }
+
+    public Integer getPermission_id() {
+        return permission_id;
+    }
+
+    public void setPermission_id(Integer permission_id) {
+        this.permission_id = permission_id;
+    }
+
+    public Date getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(Date create_time) {
+        this.create_time = create_time;
+    }
+
+    public Date getUpdate_time() {
+        return update_time;
+    }
+
+    public void setUpdate_time(Date update_time) {
+        this.update_time = update_time;
+    }
+}
