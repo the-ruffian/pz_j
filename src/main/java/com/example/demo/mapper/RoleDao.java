d057b251fd8d922613ad6f1d2ec1b31f0f7979c5
bugpz <13558622779@163.com>
2021-06-14 13:07:23 +0800

    fix：增加根据角色名查id通用查询语句，便于删除角色功能

--- src/main/java/com/example/demo/mapper/RoleDao.java
+++ src/main/java/com/example/demo/mapper/RoleDao.java
@@ -10,8 +10,12 @@ package com.example.demo.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.demo.entity.Role;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface RoleDao extends BaseMapper<Role> {
+    @Select("select id from role where role_name = #{roleName}")
+    Integer selectByRoleNameInteger(@Param("roleName") String roleName);
 }
