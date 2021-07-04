215ead80f3ccc49557f5b1f98be3e240385a84f1
bugpz <13558622779@163.com>
2021-07-04 20:15:56 +0800

    fix：修改登录及注册接口

--- src/main/java/com/example/demo/comtroller/UserController.java
+++ src/main/java/com/example/demo/comtroller/UserController.java
@@ -1,3 +1,11 @@
+/*
+ * @Description:用户模块
+ * @CreatedBy:IntelliJ IDEA
+ * @Author: the-ruffian
+ * @Date:
+ * @LastEditTime: 2021-7-4 20:15:31
+ * @LastEditors: the-ruffian
+ */
 package com.example.demo.comtroller;
 
 
@@ -5,20 +13,18 @@ import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.example.demo.entity.Token;
 import com.example.demo.entity.User;
-import com.example.demo.enums.Status;
 import com.example.demo.mapper.TokenDao;
 import com.example.demo.mapper.UserDao;
+import com.example.demo.model.dto.UserLoginDto;
 import com.example.demo.model.dto.UserRegisterDto;
-import com.example.demo.service.UserService;
-import com.example.demo.utils.JWTUtils;
+import com.example.demo.service.UserLoginService;
+import com.example.demo.service.UserRegisterService;
 import com.example.demo.utils.PublicMethod;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
-import com.example.demo.utils.model.ApiResponse;
+import com.example.demo.utils.model.OpenResponse;
 
 
 import java.util.*;
@@ -31,7 +37,9 @@ public class UserController {
     @Autowired
     private TokenDao tokenDao;
     @Autowired
-    private UserService userService;
+    private UserRegisterService userRegisterService;
+    @Autowired
+    private UserLoginService userLoginService;
 
     /*
     * 1.获取所有信息
@@ -57,37 +65,12 @@ public class UserController {
 
 
     /*
-    * 2.创建一条信息
+    * 2.注册接口
     * */
     @ApiOperation(value = "用户注册")
     @PostMapping(value = "/api/user/register", produces = "application/json;charset=UTF-8")
-    public Object create(@RequestBody JSONObject jsonParam){
-        String username = jsonParam.getString("username");
-        String password = jsonParam.getString("password");
-        String email = jsonParam.getString("email");
-        String phone = jsonParam.getString("phone");
-        Integer gender = jsonParam.getString("gender").equals("男")  ? 1 : jsonParam.getString("gender").equals("女") ? 0 : 2;
-        String md5Password = DigestUtils.md5DigestAsHex(password.getBytes());
-        QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
-        userQueryWrapper.select("phone").like("phone",phone);
-        Integer integer = userDao.selectCount(userQueryWrapper);
-        JSONObject obj = new JSONObject();
-        if (integer == 0){
-            User user = new User();
-            user.setPhone(phone);
-            user.setPassword(md5Password);
-            user.setGender(gender);
-            user.setUsername(username);
-            user.setEmail(email);
-            user.setCreateTime(PublicMethod.getNowTime());
-            userDao.insert(user);
-            obj.put("code",200);
-            obj.put("msg", "尊敬的" + username + "用户，恭喜你注册成功");
-        } else {
-            obj.put("code",400);
-            obj.put("msg","该手机号已注册");
-        }
-        return obj.toJSONString();
+    public OpenResponse create(@RequestBody UserRegisterDto userRegisterDto){
+        return userRegisterService.registerUser(userRegisterDto);
     }
 
     /*
@@ -152,66 +135,9 @@ public class UserController {
     }
 
     @ApiOperation("用户登录")
-//    @ApiResponses({
-//            @ApiResponse(code = 200, message = "请求成功"),
-//            @ApiResponse(code = 400, message = "请求参数没填好"),
-//            @ApiResponse(code = 404, message = "请求路径没有或页面跳转路径不对")
-//    })
     @PostMapping(value = "/api/user/login", produces = "application/json;charset=UTF-8")
-    public Object login(@RequestBody JSONObject jsonParam){
-        String phone = jsonParam.getString("phone");
-        String password = jsonParam.getString("password");
-        String md5Password = DigestUtils.md5DigestAsHex(password.getBytes());
-        System.out.println(phone + password);
-        QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
-        userQueryWrapper.select("password").eq("phone",phone);
-        Integer integer = userDao.selectCount(userQueryWrapper);
-        JSONObject obj = new JSONObject();
-        if (integer != 0){
-            QueryWrapper<User> userQueryWrapper1 = new QueryWrapper<>();
-            userQueryWrapper1.select("username","password").eq("phone",phone);
-            User user = userDao.selectOne(userQueryWrapper1);
-            if (md5Password.equals(user.getPassword())){
-                Map<String, String> payload = new HashMap<>();
-                payload.put("phone", phone);
-                payload.put("md5Password", md5Password);
-                String token = JWTUtils.getToken(payload);
-                QueryWrapper<Token> tokenQueryWrapper = new QueryWrapper<>();
-                tokenQueryWrapper.select("id").eq("phone",phone);
-                Integer integer1 = tokenDao.selectCount(tokenQueryWrapper);
-                if (integer1 == 1){
-                    Token tokenList = tokenDao.selectOne(tokenQueryWrapper);
-                    Integer tokenId = tokenList.getId();
-                    Token token1 = new Token();
-                    token1.setId(tokenId);
-                    token1.setToken(token);
-                    tokenDao.updateById(token1);
-                } else {
-                    Token token1 = new Token();
-                    token1.setPhone(phone);
-                    token1.setToken(token);
-                    tokenDao.insert(token1);
-                }
-                user.setLoginTime(PublicMethod.getNowTime());
-                userDao.update(user, userQueryWrapper.eq("phone",phone));
-                obj.put("code", 200);
-                obj.put("msg", "登录成功");
-                obj.put("token", token);
-                obj.put("username", user.getUsername());
-            } else {
-                obj.put("code", 201);
-                obj.put("msg", "密码错误");
-            }
-        }else {
-            obj.put("code",401);
-            obj.put("msg","账号不存在");
-        }
-        return obj.toJSONString();
-    }
+    public OpenResponse login(@RequestBody UserLoginDto userLoginDto){
 
-    @PostMapping(value = "/register", produces = "application/json;charset=UTF-8")
-    public Object registerUser(@RequestBody  UserRegisterDto userRegisterDto) {
-        userService.registerUser(userRegisterDto);
-        return ApiResponse.ok("成功");
+        return userLoginService.login(userLoginDto);
     }
 }
