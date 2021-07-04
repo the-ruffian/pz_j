0b9c31f82780e55bf165eeb17483868b0805801e
bugpz <13558622779@163.com>
2021-07-04 10:04:38 +0800

    fix：修改文件名，避免和swagger包重名

---
+++ src/main/java/com/example/demo/utils/model/OpenResponse.java
@@ -0,0 +1,44 @@
+/*
+ * @Description:OpenRespone
+ * @CreatedBy:IntelliJ IDEA
+ * @Author: the-ruffian
+ * @Date: 2021-07-02 11:25
+ * @LastEditTime: 2021-7-4 10:03:30
+ * @LastEditors: the-ruffian
+ */
+package com.example.demo.utils.model;
+
+import com.example.demo.enums.Status;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@ApiModel("JSON响应数据")
+@Data
+public class OpenResponse<T> extends BaseResponse implements Serializable {
+
+    @ApiModelProperty("返回数据")
+    private T result;
+
+    public static <T> OpenResponse<T> ok(String message) {
+        OpenResponse<T> openResponse = new OpenResponse<>();
+        openResponse.setSuccess(true);
+        openResponse.setCode(Status.SUCCESS.getCode());
+        openResponse.setMessage(message);
+        return openResponse;
+    }
+    public static <T> OpenResponse<T> ok(String message, T result){
+        return ok("ok", result);
+    }
+
+    public static <T> OpenResponse<T> ok(Status status){
+        OpenResponse<T> openResponse = new OpenResponse<>();
+        openResponse.setSuccess(true);
+        openResponse.setCode(status.getCode());
+        openResponse.setMessage(status.getMessage());
+        openResponse.setResult(openResponse.result);
+        return openResponse;
+    }
+}
