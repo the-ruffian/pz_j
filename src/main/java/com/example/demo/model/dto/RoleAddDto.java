3ee6316792263267c240ef3b2edf820eace96cb1
bugpz <13558622779@163.com>
2021-07-09 21:51:22 +0800

    fix：添加Data注解

--- src/main/java/com/example/demo/model/dto/RoleAddDto.java
+++ src/main/java/com/example/demo/model/dto/RoleAddDto.java
@@ -1,15 +1,17 @@
 /*
- * @Description:UserDeleteService
+ * @Description:新增角色请求参数
  * @CreatedBy:IntelliJ IDEA
  * @Author: the-ruffian
  * @Date: 2021-07-07 20:55
- * @LastEditTime: 2021-07-07 20:55
+ * @LastEditTime: 2021-07-09 21:50:29
  * @LastEditors: the-ruffian
  */
 package com.example.demo.model.dto;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
+@Data
 public class RoleAddDto {
     @ApiModelProperty(value = "角色名")
     private String roleName;
