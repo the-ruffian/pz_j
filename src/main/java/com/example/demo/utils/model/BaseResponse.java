8031fde29cd2dab4503ce826ca48cc966d1edacd
bugpz <13558622779@163.com>
2021-07-09 22:44:20 +0800

    fix：添加Data注解，删除多余代码

--- src/main/java/com/example/demo/utils/model/BaseResponse.java
+++ src/main/java/com/example/demo/utils/model/BaseResponse.java
@@ -3,7 +3,7 @@
  * @CreatedBy:IntelliJ IDEA
  * @Author: the-ruffian
  * @Date: 2021-07-02 09:48
- * @LastEditTime: 2021-7-4 14:45:11
+ * @LastEditTime: 2021-07-09 22:43:52
  * @LastEditors: the-ruffian
  */
 package com.example.demo.utils.model;
@@ -11,7 +11,10 @@ package com.example.demo.utils.model;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
+
+@Data
 @ApiModel("响应对象数据")
 public class BaseResponse {
     @ApiModelProperty("是否成功")
@@ -22,45 +25,4 @@ public class BaseResponse {
     protected int subCode;
     @ApiModelProperty("返回信息")
     protected String message;
-
-
-    public static BaseResponse fail(int code, String message){
-        BaseResponse baseResponse = new BaseResponse();
-        baseResponse.setCode(code);
-        baseResponse.setSuccess(false);
-        baseResponse.setMessage(message);
-        return baseResponse;
-    }
-
-    public boolean isSuccess() {
-        return success;
-    }
-
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public int getSubCode() {
-        return subCode;
-    }
-
-    public void setSubCode(int subCode) {
-        this.subCode = subCode;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
