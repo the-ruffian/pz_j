55e64f5bdb7662aa7eb56cf9c5fb2ad13dfc41d6
bugpz <13558622779@163.com>
2021-07-13 14:56:56 +0800

    feat：用户列表入参

---
+++ src/main/java/com/example/demo/model/dto/UserListDto.java
@@ -0,0 +1,24 @@
+/*
+ * @Description:UserListDto
+ * @CreatedBy:IntelliJ IDEA
+ * @Author: the-ruffian
+ * @Date: 2021-07-13 12:58
+ * @LastEditTime: 2021-7-13 14:56:11
+ * @LastEditors: the-ruffian
+ */
+package com.example.demo.model.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+public class UserListDto {
+    @ApiModelProperty(value = "手机号")
+    private String phone;
+    @ApiModelProperty(value = "用户名")
+    private String username;
+    @ApiModelProperty(value = "邮箱")
+    private String email;
+    @ApiModelProperty(value = "性别")
+    private Integer gender;
+}
