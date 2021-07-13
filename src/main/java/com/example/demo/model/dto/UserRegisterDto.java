f57f820dbcc8b0eefd2cad2bc350aed6b723d027
bugpz <13558622779@163.com>
2021-07-13 13:00:49 +0800

    refactor：添加Data注解

--- src/main/java/com/example/demo/model/dto/UserRegisterDto.java
+++ src/main/java/com/example/demo/model/dto/UserRegisterDto.java
@@ -9,7 +9,9 @@
 package com.example.demo.model.dto;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
+@Data
 public class UserRegisterDto {
     @ApiModelProperty("用户名")
     private String username;
@@ -22,43 +24,4 @@ public class UserRegisterDto {
     @ApiModelProperty(value = "邮箱地址")
     private String email;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Integer getGender() {
-        return gender;
-    }
-
-    public void setGender(Integer gender) {
-        this.gender = gender;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
