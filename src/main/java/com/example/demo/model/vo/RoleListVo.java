427c3cc4ee720714e3741624d74650dfe0c9d6a7
bugpz <13558622779@163.com>
2021-07-14 15:42:27 +0800

    feat：角色列表Vo

---
+++ src/main/java/com/example/demo/model/vo/RoleListVo.java
@@ -0,0 +1,26 @@
+/*
+ * @Description:角色列表响应
+ * @CreatedBy:IntelliJ IDEA
+ * @Author: the-ruffian
+ * @Date: 2021-07-14 13:43
+ * @LastEditTime: 2021-7-14 15:40:48
+ * @LastEditors: the-ruffian
+ */
+package com.example.demo.model.vo;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+public class RoleListVo {
+    @ApiModelProperty(value = "角色名称")
+    private String roleName;
+    @ApiModelProperty(value = "备注")
+    private String note;
+    @ApiModelProperty(value = "创建时间")
+    private Timestamp createTime;
+    @ApiModelProperty(value = "修改时间")
+    private Timestamp updateTime;
+}
