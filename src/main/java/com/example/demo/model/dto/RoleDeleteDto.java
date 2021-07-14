219e7f761b43d16a7f0d8bfaec0c94c2d01a78f5
bugpz <13558622779@163.com>
2021-07-14 19:35:06 +0800

    refactor：加Data注解

--- src/main/java/com/example/demo/model/dto/RoleDeleteDto.java
+++ src/main/java/com/example/demo/model/dto/RoleDeleteDto.java
@@ -3,13 +3,15 @@
  * @CreatedBy:IntelliJ IDEA
  * @Author: the-ruffian
  * @Date: 2021-07-14 19:19
- * @LastEditTime: 2021-07-14 19:19
+ * @LastEditTime: 2021-7-14 19:34:21
  * @LastEditors: the-ruffian
  */
 package com.example.demo.model.dto;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
+@Data
 public class RoleDeleteDto {
     @ApiModelProperty(value = "角色名")
     private Integer roleId;
