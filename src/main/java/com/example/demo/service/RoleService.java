41fd629c496bf050b0705843afbaf6d4c2775b74
bugpz <13558622779@163.com>
2021-07-14 19:50:22 +0800

    refactor：角色模块代码重构

---
+++ src/main/java/com/example/demo/service/RoleService.java
@@ -0,0 +1,23 @@
+/*
+ * @Description:新增角色
+ * @CreatedBy:IntelliJ IDEA
+ * @Author: the-ruffian
+ * @Date: 2021-07-09 21:38:32
+ * @LastEditTime: 2021-7-14 19:49:40
+ * @LastEditors: the-ruffian
+ */
+package com.example.demo.service;
+
+import com.example.demo.model.dto.RoleAddDto;
+import com.example.demo.model.dto.RoleDeleteDto;
+import com.example.demo.model.dto.RoleListDto;
+import com.example.demo.model.dto.RoleUpdateDto;
+import com.example.demo.utils.model.OpenResponse;
+import org.apache.ibatis.annotations.Param;
+
+public interface RoleService {
+    OpenResponse add(@Param("roleAddDto")RoleAddDto roleAddDto);
+    OpenResponse search(@Param("roleListDto")RoleListDto roleListDto);
+    OpenResponse update(@Param("roleUpdateDto")RoleUpdateDto roleUpdateDto);
+    OpenResponse delete(@Param("roleDeleteDto")RoleDeleteDto roleDeleteDto);
+}
