d8bb2007747dbd1536a028903782ae13b5e2a042
bugpz <13558622779@163.com>
2021-07-18 18:29:43 +0800

    refactor：重构菜单模块

--- src/main/java/com/example/demo/comtroller/MenuController.java
+++ src/main/java/com/example/demo/comtroller/MenuController.java
@@ -1,16 +1,17 @@
 /*
- * @Description:MenuController
+ * @Description:菜单
  * @CreatedBy:IntelliJ IDEA
  * @Author: the-ruffian
  * @Date: 2021-06-02 16:22
- * @LastEditTime: 2021-06-02 16:22
+ * @LastEditTime: 2021-07-18 18:28:40
  * @LastEditors: the-ruffian
  */
 package com.example.demo.comtroller;
 
-import com.alibaba.fastjson.JSONObject;
-import com.example.demo.entity.Permission;
 import com.example.demo.mapper.UserRoleDao;
+import com.example.demo.model.dto.UserMenuListDto;
+import com.example.demo.service.MenuService;
+import com.example.demo.utils.model.OpenResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,73 +19,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
+
+
 
 @RestController
 @Api(tags = "菜单权限")
 public class MenuController {
     @Autowired
     UserRoleDao userRoleDao;
+    @Autowired
+    MenuService menuService;
 
     @ApiOperation(value = "获取菜单权限")
     @PostMapping(value = "/api/auth/list",produces = "application/json;charset=UTF-8")
-    public Object menu(@RequestBody JSONObject jsonParam){
-        String phone = jsonParam.getString("phone");
-        List<Permission> allMenu = userRoleDao.selectByPhone(phone);
-        JSONObject obj = new JSONObject();
-//        allMenu.forEach(i -> {
-//            if (i.getLevel().equals("1")) {
-//                for (Permission child: allMenu){
-//                    if (child.getLevel().equals("2") && child.getParentId().equals(i.getId())){
-//                        PermissionVo permissionVo = new PermissionVo();
-//                        for (Permission children:allMenu){
-//                            if (children.getLevel().equals("3") && children.getParentId().equals(child.getId())) {
-//                                PermissionVo permissionVo1 = new PermissionVo();
-//                                BeanUtils.copyProperties(children, permissionVo1);
-//                                permissionThree.add(permissionVo1);
-//                            }
-//                            permissionVo.setChild(permissionThree);
-//                        }
-//                        BeanUtils.copyProperties(child, permissionVo);
-//                        permissionTwo.add(permissionVo);
-//                    }
-//                }
-//
-//                PermissionVo permissionVo = new PermissionVo();
-//                permissionVo.setChild(permissionTwo);
-//                BeanUtils.copyProperties(i, permissionVo);
-//                permissionVoList.add(permissionVo);
-//            }
-//        });
-        ArrayList<Permission> permissions = new ArrayList<>();
-        ArrayList<Permission> fPermission = new ArrayList<>();
-        ArrayList<Permission> childPermission = new ArrayList<>();
-        for (Permission top:allMenu){
-            if (top.getParentId().equals(0) && top.getLevel().equals("1")){
-                JSONObject childOBJ = new JSONObject();
-                for (Permission child:allMenu){
-                    if (child.getParentId().equals(top.getId())){
-                        JSONObject childrenOBJ = new JSONObject();
-                        for (Permission children:allMenu){
-                            if (children.getParentId().equals(child.getId())){
-                                permissions.add(children);
-                                childrenOBJ.put("result",permissions);
-                                childrenOBJ.toJSONString();
-                            }
-                        }
-                        childPermission.add(child);
-                        childOBJ.put("result",childPermission);
-                        childOBJ.put("child",childrenOBJ);
-                        childOBJ.toJSONString();
-                    }
-                }
-                fPermission.add(top);
-                obj.put("code",200);
-                obj.put("result",fPermission);
-                obj.put("child",childOBJ);
-            }
-        }
-        return obj.toJSONString();
+    public OpenResponse menu(@RequestBody UserMenuListDto userMenuListDto){
+        return menuService.userMenu(userMenuListDto);
     }
 }
