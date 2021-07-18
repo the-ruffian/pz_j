6f67491a1355f2eb0f4017ac57235a3e5e57167a
bugpz <13558622779@163.com>
2021-07-18 18:26:38 +0800

    add：菜单权限服务实现类

---
+++ src/main/java/com/example/demo/service/impl/MenuServiceImpl.java
@@ -0,0 +1,46 @@
+//2021-07-18 16:46:24
+package com.example.demo.service.impl;
+
+import com.example.demo.mapper.PermissionDao;
+import com.example.demo.model.dto.UserMenuListDto;
+import com.example.demo.model.vo.UserMenuListVo;
+import com.example.demo.service.MenuService;
+import com.example.demo.utils.model.OpenResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service("MenuService")
+public class MenuServiceImpl implements MenuService {
+
+    @Autowired
+    PermissionDao permissionDao;
+
+    @Override
+    public OpenResponse userMenu(UserMenuListDto userMenuListDto){
+        List<UserMenuListVo> permissions = permissionDao.allMenu(userMenuListDto);
+        ArrayList<UserMenuListVo> userMenuListVos = new ArrayList<>();
+        ArrayList<UserMenuListVo> userMenuListVos1 = new ArrayList<>();
+        List<UserMenuListVo> userMenuListVo2 = new ArrayList<>();
+        permissions.forEach(i -> {
+            if (i.getLevel()==1){
+                for (UserMenuListVo child:permissions ){
+                    if (child.getParentId()==i.getId()){
+                        for (UserMenuListVo children:permissions){
+                            if (children.getParentId()==child.getId()){
+                                userMenuListVo2.add(children);
+                            }
+                        }
+                        child.setChildren(userMenuListVo2);
+                        userMenuListVos1.add(child);
+                    }
+                }
+            i.setChildren(userMenuListVos1);
+            userMenuListVos.add(i);
+            }
+        });
+        return OpenResponse.ok("菜单权限",userMenuListVos);
+    }
+}
