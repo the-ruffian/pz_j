5ce46175ce36176e7df11d16833c93b7cfc71b60
bugpz <13558622779@163.com>
2021-08-14 17:30:13 +0800

    add：验证码表映射

---
+++ src/main/java/com/example/demo/entity/Sys_code.java
@@ -0,0 +1,31 @@
+/*
+ * @Description:Sys_code
+ * @CreatedBy:IntelliJ IDEA
+ * @Author: the-ruffian
+ * @Date: 2021-08-14 17:25
+ * @LastEditTime: 2021-08-14 17:25
+ * @LastEditors: the-ruffian
+ */
+package com.example.demo.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+@TableName(value = "sys_code")
+public class Sys_code {
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    private Integer used;
+
+    private String email;
+
+    private Timestamp createTime;
+
+    private Timestamp UpdateTime;
+}
