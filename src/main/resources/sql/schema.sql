373bad763823c7df064aeda98fa952076efdbeb7
bugpz <13558622779@163.com>
2021-08-14 17:23:33 +0800

    add：sql增加

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -79,3 +79,16 @@ create table if not exists `role_permission`
 ) engine = InnoDB
   default
       charset = utf8 comment '角色权限表';
+
+#验证码表
+create table if not exists `sys_code`
+(
+    `id`          bigint(20) unsigned not null auto_increment comment '主键',
+    `phone`       varchar(12)         not null comment '电话',
+    `used`        tinyint(1) default 0 comment '使用状态 0未使用1已使用',
+    `email`       varchar(30) comment '邮箱',
+    `create_time` datetime comment '创建时间',
+    `update_time` datetime comment '修改时间',
+    primary key (`id`)
+) engine = InnoDB
+  default charset = utf8 comment '验证码表';
