c222c43b305c836d06945e6642d4a8a633bf784e
bugpz <13558622779@163.com>
2021-08-15 15:56:24 +0800

    fix：字段修改

--- src/main/java/com/example/demo/model/dto/UserResetPasswordDto.java
+++ src/main/java/com/example/demo/model/dto/UserResetPasswordDto.java
@@ -3,7 +3,7 @@
  * @CreatedBy:IntelliJ IDEA
  * @Author: the-ruffian
  * @Date: 2021-07-24 20:52:51
- * @LastEditTime: 2021-07-24 20:55:11
+ * @LastEditTime: 2021-08-15 15:56:08
  * @LastEditors: the-ruffian
  */
 package com.example.demo.model.dto;
@@ -14,10 +14,12 @@ import lombok.Data;
 @Data
 public class UserResetPasswordDto {
 
+    @ApiModelProperty(value = "验证码")
+    private String code;
     @ApiModelProperty(value = "新密码")
     private String password;
     @ApiModelProperty(value = "确认密码")
     private String RePassword;
-    @ApiModelProperty(value = "手机号")
-    private String phone;
+    @ApiModelProperty(value = "邮箱")
+    private String email;
 }
