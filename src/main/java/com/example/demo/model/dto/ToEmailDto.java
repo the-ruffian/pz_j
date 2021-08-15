3fb4882a4e4ec58e1b0bf126f6bf471743fb6e42
bugpz <13558622779@163.com>
2021-08-15 18:41:01 +0800

    fix：字段类型修改

--- src/main/java/com/example/demo/model/dto/ToEmailDto.java
+++ src/main/java/com/example/demo/model/dto/ToEmailDto.java
@@ -3,7 +3,7 @@
  * @CreatedBy:IntelliJ IDEA
  * @Author: the-ruffian
  * @Date: 2021-08-14 16:38
- * @LastEditTime: 2021-08-14 16:38
+ * @LastEditTime: 2021-08-15 18:40:51
  * @LastEditors: the-ruffian
  */
 package com.example.demo.model.dto;
@@ -12,5 +12,5 @@ import lombok.Data;
 
 @Data
 public class ToEmailDto {
-    private String[] tos;
+    private String tos;
 }
