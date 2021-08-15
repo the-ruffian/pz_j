6fad19eeb378848067c53169250e2e27b95d00f9
bugpz <13558622779@163.com>
2021-08-15 16:15:16 +0800

    fix：加注解

--- src/main/java/com/example/demo/mapper/SysCodeDao.java
+++ src/main/java/com/example/demo/mapper/SysCodeDao.java
@@ -3,13 +3,15 @@
  * @CreatedBy:IntelliJ IDEA
  * @Author: the-ruffian
  * @Date: 2021-08-15 16:10
- * @LastEditTime: 2021-08-15 16:10
+ * @LastEditTime: 2021-08-15 16:15:12
  * @LastEditors: the-ruffian
  */
 package com.example.demo.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.demo.entity.Sys_code;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface SysCodeDao extends BaseMapper<Sys_code> {
 }
