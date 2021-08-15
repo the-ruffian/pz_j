e363e3dce0283954180a6a0b8183955520230098
bugpz <13558622779@163.com>
2021-08-15 18:08:18 +0800

    fix：验证码生成方式修改

--- src/main/java/com/example/demo/service/impl/MailServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/MailServiceImpl.java
@@ -1,15 +1,21 @@
 /*
- * @Description:MailServiceImpl
+ * @Description:邮件服务
  * @CreatedBy:IntelliJ IDEA
  * @Author: the-ruffian
  * @Date: 2021-08-14 16:41
- * @LastEditTime: 2021-08-14 16:41
+ * @LastEditTime: 2021-08-15 18:07:00
  * @LastEditors: the-ruffian
  */
 package com.example.demo.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.demo.entity.Sys_code;
+import com.example.demo.entity.User;
+import com.example.demo.mapper.SysCodeDao;
+import com.example.demo.mapper.UserDao;
 import com.example.demo.model.dto.ToEmailDto;
 import com.example.demo.service.MailService;
+import com.example.demo.utils.PublicMethod;
 import com.example.demo.utils.model.OpenResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,31 +24,53 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+
+
 @Service("MailService")
 public class MailServiceImpl implements MailService {
 
     @Autowired
     private JavaMailSender mailSender;
+
+    @Autowired
+    UserDao userDao;
+    @Autowired
+    SysCodeDao sysCodeDao;
     @Value("${spring.mail.username}")
     private String from;
+
+
     @Override
     public OpenResponse toEmail(ToEmailDto toEmailDto) {
-        //创建简单邮件消息
-        SimpleMailMessage message = new SimpleMailMessage();
-        //谁发的
-        message.setFrom(from);
-        //给谁发
-        message.setTo(toEmailDto.getTos());
-        //邮件标题
-        message.setSubject("验证码");
-        //邮件类容
-        message.setText("1234");
-        try {
-            mailSender.send(message);
-            return OpenResponse.ok("邮件发送成功");
-        } catch (MailException e) {
-            e.printStackTrace();
-            return OpenResponse.fail("邮件发送失败");
+        QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
+        Integer userMail = userDao.selectCount(userQueryWrapper.eq("email",toEmailDto.getTos()));
+        if (userMail==1){
+            String code = PublicMethod.code().toString();
+            Sys_code sys_code = new Sys_code();
+            sys_code.setEmail(toEmailDto.getTos());
+            sys_code.setCode(code);
+            sys_code.setCreateTime(PublicMethod.getNowTime());
+            sysCodeDao.insert(sys_code);
+            //创建简单邮件消息
+            SimpleMailMessage message = new SimpleMailMessage();
+            //谁发的
+            message.setFrom(from);
+            //给谁发
+            message.setTo(toEmailDto.getTos());
+            //邮件标题
+            message.setSubject("验证码");
+            //邮件类容
+            message.setText(code);
+            try {
+                mailSender.send(message);
+                return OpenResponse.ok("邮件发送成功");
+            } catch (MailException e) {
+                e.printStackTrace();
+                return OpenResponse.fail("邮件发送失败");
+            }
+        }else {
+            return OpenResponse.fail(toEmailDto.getTos()+"在系统中不存在,请重新输入");
         }
+
     }
 }
