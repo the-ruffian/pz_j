374ecc1f1b69dfde7cb6ea1a9d3f885cb28461d9
bugpz <13558622779@163.com>
2021-08-15 16:10:59 +0800

    fix：Dao

---
+++ src/main/java/com/example/demo/mapper/SysCodeDao.java
@@ -0,0 +1,15 @@
+/*
+ * @Description:验证码Dao
+ * @CreatedBy:IntelliJ IDEA
+ * @Author: the-ruffian
+ * @Date: 2021-08-15 16:10
+ * @LastEditTime: 2021-08-15 16:10
+ * @LastEditors: the-ruffian
+ */
+package com.example.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.demo.entity.Sys_code;
+
+public interface SysCodeDao extends BaseMapper<Sys_code> {
+}
