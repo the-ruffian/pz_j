a6af2606881aff9bfac46297463700d90e6f342c
bugpz <13558622779@163.com>
2021-08-23 21:01:16 +0800

    add：修改角色状态接口

--- src/main/java/com/example/demo/service/RoleService.java
+++ src/main/java/com/example/demo/service/RoleService.java
@@ -3,15 +3,12 @@
  * @CreatedBy:IntelliJ IDEA
  * @Author: the-ruffian
  * @Date: 2021-07-09 21:38:32
- * @LastEditTime: 2021-08-21 18:05:21
+ * @LastEditTime: 2021-8-23 21:00:20
  * @LastEditors: the-ruffian
  */
 package com.example.demo.service;
 
-import com.example.demo.model.dto.RoleAddDto;
-import com.example.demo.model.dto.RoleDeleteDto;
-import com.example.demo.model.dto.RoleListDto;
-import com.example.demo.model.dto.RoleUpdateDto;
+import com.example.demo.model.dto.*;
 import com.example.demo.utils.model.OpenResponse;
 import org.apache.ibatis.annotations.Param;
 
@@ -21,4 +18,5 @@ public interface RoleService {
     OpenResponse update(@Param("roleUpdateDto")RoleUpdateDto roleUpdateDto);
     OpenResponse delete(@Param("roleDeleteDto")RoleDeleteDto roleDeleteDto);
     OpenResponse permission();
+    OpenResponse fixstatus(@Param("roleStatusDto")RoleStatusDto roleStatusDto);
 }
