beed8885247a369b0593404f31867b83d2943fa5
bugpz <13558622779@163.com>
2021-09-25 23:10:52 +0800

    fix：规范代码

--- src/main/java/com/example/demo/model/dto/RoleDeleteDto.java
+++ src/main/java/com/example/demo/model/dto/RoleDeleteDto.java
@@ -1,16 +1,12 @@
-/*
- * @Description:删除角色入参
- * @CreatedBy:IntelliJ IDEA
- * @Author: the-ruffian
- * @Date: 2021-07-14 19:19
- * @LastEditTime: 2021-7-14 19:34:21
- * @LastEditors: the-ruffian
- */
 package com.example.demo.model.dto;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+/**
+ * @author bugpz
+ * @date 2021-09-25 23:09:16
+ */
 @Data
 public class RoleDeleteDto {
     @ApiModelProperty(value = "角色名")
