e802617ce968d183347ac7e28835cde3b11626bf
bugpz <13558622779@163.com>
2021-09-25 23:08:42 +0800

    fix：规范代码

--- src/main/java/com/example/demo/model/dto/RoleAddDto.java
+++ src/main/java/com/example/demo/model/dto/RoleAddDto.java
@@ -1,16 +1,12 @@
-/*
- * @Description:新增角色请求参数
- * @CreatedBy:IntelliJ IDEA
- * @Author: the-ruffian
- * @Date: 2021-07-07 20:55
- * @LastEditTime: 2021-07-09 21:50:29
- * @LastEditors: the-ruffian
- */
 package com.example.demo.model.dto;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+/**
+ * @author bugpz
+ * @date 2021-09-25 23:08:26
+ */
 @Data
 public class RoleAddDto {
     @ApiModelProperty(value = "角色名")
