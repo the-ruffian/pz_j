b318c6ecb95b308bfcef32bffc19b980ae90b42c
bugpz <13558622779@163.com>
2021-09-25 22:33:59 +0800

    fix：规范代码

---
+++ src/main/java/com/example/demo/entity/UserRole.java
@@ -0,0 +1,33 @@
+package com.example.demo.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import java.sql.Timestamp;
+
+/**
+ * @author bugpz
+ * @date 2021-09-25 22:31:17
+ */
+@Data
+@TableName(value = "user_role")
+public class UserRole {
+
+    @ApiModelProperty(value = "id")
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    @ApiModelProperty(value = "用户id")
+    private Integer userId;
+
+    @ApiModelProperty(value = "角色id")
+    private Integer roleId;
+
+    @ApiModelProperty(value = "创建时间")
+    private Timestamp createTime;
+
+    @ApiModelProperty(value = "更新时间")
+    private Timestamp updateTime;
+}
