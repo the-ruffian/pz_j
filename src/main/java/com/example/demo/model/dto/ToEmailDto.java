d0aebc517856969cbab043f48be9e3eaddd36381
bugpz <13558622779@163.com>
2021-09-25 23:11:05 +0800

    fix：规范代码

--- src/main/java/com/example/demo/model/dto/ToEmailDto.java
+++ src/main/java/com/example/demo/model/dto/ToEmailDto.java
@@ -1,15 +1,11 @@
-/*
- * @Description:邮件入参
- * @CreatedBy:IntelliJ IDEA
- * @Author: the-ruffian
- * @Date: 2021-08-14 16:38
- * @LastEditTime: 2021-08-15 18:40:51
- * @LastEditors: the-ruffian
- */
 package com.example.demo.model.dto;
 
 import lombok.Data;
 
+/**
+ * @author bugpz
+ * @date 2021-09-25 23:10:41
+ */
 @Data
 public class ToEmailDto {
     private String tos;
