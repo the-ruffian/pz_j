dc16beda513d42f0559ebaeae425dbb369a4c942
bugpz <13558622779@163.com>
2021-11-17 15:13:48 +0800

    add：栏目服务

---
+++ src/main/java/com/example/demo/comtroller/ColumnMenuController.java
@@ -0,0 +1,29 @@
+package com.example.demo.comtroller;
+
+import com.example.demo.model.dto.article.ColumnMenuDto;
+import com.example.demo.service.Column.ColumnService;
+import com.example.demo.utils.model.OpenResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(tags = "栏目")
+public class ColumnMenuController {
+    @Autowired
+    ColumnService columnService;
+
+    /**
+     * @Description 新增栏目
+     * @param columnMenuDto
+     * @return
+     */
+    @ApiOperation(value = "新增栏目")
+    @PostMapping(value = "/api/columnMenu/add",produces = "application/json;charset=UTF-8")
+    public OpenResponse add(@RequestBody ColumnMenuDto columnMenuDto) {
+        return columnService.addColumn(columnMenuDto);
+    }
+}
