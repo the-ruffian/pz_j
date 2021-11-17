8e6f5b3b68654950245073836618a2326237d64d
bugpz <13558622779@163.com>
2021-11-17 15:14:59 +0800

    add：栏目Dao

---
+++ src/main/java/com/example/demo/mapper/ColumnMenuDao.java
@@ -0,0 +1,10 @@
+package com.example.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.demo.entity.ColumnMenu;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface ColumnMenuDao extends BaseMapper<ColumnMenu> {
+
+}
