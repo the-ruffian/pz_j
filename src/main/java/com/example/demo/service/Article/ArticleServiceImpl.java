e74960be3487dc951458a4adb0a94c808056a99b
bugpz <13558622779@163.com>
2021-12-28 22:08:25 +0800

    fix：增加参数处理

--- src/main/java/com/example/demo/service/Article/ArticleServiceImpl.java
+++ src/main/java/com/example/demo/service/Article/ArticleServiceImpl.java
@@ -9,21 +9,33 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * @author TR
+ */
 @Service("Article")
 public class ArticleServiceImpl implements ArticleService{
     @Autowired
     ArticleDao articleDao;
 
+    /**
+     *
+     * @param articleAddDto: title,author,article,sortId
+     * @return message
+     */
+
     @Override
     public Object newArticle(@Param("articleAddDto") ArticleAddDto articleAddDto){
+
         if (articleAddDto == null){
             return OpenResponse.fail("参数不能为空");
-        }else if (articleAddDto.getTitle() ==null || articleAddDto.getTitle().equals("")){
+        }else if (articleAddDto.getTitle() ==null || "".equals(articleAddDto.getTitle())){
             return OpenResponse.fail("标题不能为空");
-        }else if (articleAddDto.getAuthor()==null || articleAddDto.getAuthor().equals("")){
+        }else if (articleAddDto.getAuthor()==null || "".equals(articleAddDto.getAuthor())){
             return OpenResponse.fail("作者不能为空");
-        }else if (articleAddDto.getArticle() ==null || articleAddDto.getArticle().equals("")){
+        }else if (articleAddDto.getArticle() ==null || "".equals(articleAddDto.getArticle())){
             return OpenResponse.fail("内容不能为空");
+        }else if (articleAddDto.getSortId()==null){
+            return OpenResponse.fail("栏目id不能为空");
         }else {
             Article article = new Article();
             article.setTitle(articleAddDto.getTitle());
@@ -31,6 +43,7 @@ public class ArticleServiceImpl implements ArticleService{
             article.setArticle(articleAddDto.getArticle());
             article.setCreateTime(PublicMethod.getNowTime());
             article.setUpdateTime(PublicMethod.getNowTime());
+            article.setSortId(articleAddDto.getSortId());
             articleDao.insert(article);
             return OpenResponse.ok("新增文章<"+articleAddDto.getTitle()+">成功");
         }
