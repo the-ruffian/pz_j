99db392b7e0e32c8e61cb6ef84929ce93d845b78
bugpz <13558622779@163.com>
2021-12-30 14:24:40 +0800

    fix: 增加文章搜索接口和方法

--- src/main/java/com/example/demo/service/Article/ArticleService.java
+++ src/main/java/com/example/demo/service/Article/ArticleService.java
@@ -1,8 +1,10 @@
 package com.example.demo.service.Article;
 
 import com.example.demo.model.dto.article.ArticleAddDto;
+import com.example.demo.model.dto.article.ArticleListDto;
 import org.apache.ibatis.annotations.Param;
 
 public interface ArticleService {
     Object newArticle(@Param("articleAddDto")ArticleAddDto articleAddDto);
+    Object searchArticle(@Param("articleListDto")ArticleListDto articleListDto);
 }
--- src/main/java/com/example/demo/service/Article/ArticleServiceImpl.java
+++ src/main/java/com/example/demo/service/Article/ArticleServiceImpl.java
@@ -1,13 +1,20 @@
 package com.example.demo.service.Article;
 
 import com.example.demo.entity.Article;
+import com.example.demo.enums.Status;
 import com.example.demo.mapper.ArticleDao;
 import com.example.demo.model.dto.article.ArticleAddDto;
+import com.example.demo.model.dto.article.ArticleListDto;
+import com.example.demo.model.vo.article.ArticleListVo;
 import com.example.demo.utils.PublicMethod;
 import com.example.demo.utils.model.OpenResponse;
+import com.github.pagehelper.PageInfo;
+import com.github.pagehelper.page.PageMethod;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author TR
@@ -47,5 +54,12 @@ public class ArticleServiceImpl implements ArticleService{
             articleDao.insert(article);
             return OpenResponse.ok("新增文章<"+articleAddDto.getTitle()+">成功");
         }
+    }
+
+    @Override
+    public Object searchArticle(ArticleListDto articleListDto) {
+        PageMethod.startPage(articleListDto.getPageNo(),articleListDto.getPageSize());
+        List<ArticleListVo> articleListVos = articleDao.search(articleListDto);
+        return OpenResponse.ok(Status.SUCCESS.getMessage(),new PageInfo<>(articleListVos));
     }
 }
