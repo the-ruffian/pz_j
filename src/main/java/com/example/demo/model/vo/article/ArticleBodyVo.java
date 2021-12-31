732b69446c23d47f7cc5df1be8fcf55335bf9e39
bugpz <13558622779@163.com>
2021-12-31 14:07:32 +0800

    feat: 文章内容出参

---
+++ src/main/java/com/example/demo/model/vo/article/ArticleBodyVo.java
@@ -0,0 +1,27 @@
+package com.example.demo.model.vo.article;
+
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+public class ArticleBodyVo {
+    @ApiModelProperty(value = "标题")
+    private String title;
+    @ApiModelProperty(value = "作者")
+    private String author;
+    @ApiModelProperty(value = "内容")
+    private String article;
+    @ApiModelProperty(value = "创建时间")
+    private Timestamp createTime;
+    @ApiModelProperty(value = "修改时间")
+    private Timestamp updateTime;
+    @ApiModelProperty(value = "点击数")
+    private Integer hits;
+    @ApiModelProperty(value = "评论数")
+    private Integer postNum;
+    @ApiModelProperty(value = "栏目")
+    private String columnName;
+}
