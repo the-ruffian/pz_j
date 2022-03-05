97b6c0073c49d1706506729ad44d7b4150bceb35
bugpz <13558622779@163.com>
2022-03-05 18:34:44 +0800

    feat：新增白名单接口

--- src/main/java/com/example/demo/config/InterceptorConfig.java
+++ src/main/java/com/example/demo/config/InterceptorConfig.java
@@ -5,13 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * @author bugpz
+ */
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new JWTInterceptor())
-                .addPathPatterns("/api/**") //token验证接口
-                .excludePathPatterns("/api/user/login","/api/user/register","/api/getCode","/api/forgetPassword",  //放行接口
-                        "/swagger-ui.html/**", "/v2/**","/swagger-resources/**","/webjars/**","/*.html"); //swagger相关的接口
+                //token验证接口
+                .addPathPatterns("/api/**")
+                //放行接口
+                .excludePathPatterns("/api/user/login","/api/user/register","/api/getCode","/api/forgetPassword",
+                        "/api/columnMenu/list", "/api/article/list",
+                        "/swagger-ui.html/**", "/v2/**","/swagger-resources/**","/webjars/**","/*.html");
     }
 }
