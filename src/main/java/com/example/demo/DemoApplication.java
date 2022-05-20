d606a037b0eb38ce2f280b54df935f7bc14ac3f6
bugpz <13558622779@163.com>
2022-05-20 15:14:00 +0800

    docs: 修改注释

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,9 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*
-    @SpringBootApplication 标注一个主程序，说明这是一个springboot应用
-*/
+/**
+ * @author the-ruffian
+ *  '@SpringBootApplication' 标注一个主程序，说明这是一个springboot应用
+**/
 @SpringBootApplication
 @MapperScan("com.example.demo.mapper")
 public class DemoApplication {
